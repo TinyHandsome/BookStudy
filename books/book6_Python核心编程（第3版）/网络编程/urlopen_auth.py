b98afe88426cf3fe8af751e0081b50048232d897
公司笔记本 <694317828@qq.com>
2021-03-15 17:39:47 +0800

    学到P318

---
+++ books/book6_Python核心编程（第3版）/网络编程/urlopen_auth.py
@@ -0,0 +1,29 @@
+from base64 import encodestring
+from urllib.parse import urlparse
+from urllib.request import HTTPBasicAuthHandler, build_opener, install_opener, Request, urlopen
+
+LOGIN = 'wesley'
+PASSWD = 'you will never guess'
+URL = 'http://localhost/'
+REALM = 'Secure Archive'
+
+def handler_version(url):
+    hdlr = HTTPBasicAuthHandler()
+    hdlr.add_password(REALM, urlparse(url)[1], LOGIN, PASSWD)
+    opener = build_opener(hdlr)
+    install_opener(opener)
+    return url
+
+def request_version(url):
+    req = Request(url)
+    b64str = encodestring('%s:%s' % (LOGIN, PASSWD))[: -1]
+    req.add_header("Authorization", "Basic %s" % b64str)
+    return req
+
+for funcType in ('handler', 'request'):
+    print('*** Using %s: ' % funcType.upper())
+    url = eval('%s_version' % funcType)(URL)
+    f = urlopen(url)
+    print(f.readline())
+    f.close()
+
