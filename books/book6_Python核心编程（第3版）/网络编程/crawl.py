0a37f2065c419bca583f4cd1a961a7f05a725c9a
公司笔记本 <694317828@qq.com>
2021-04-07 17:17:36 +0800

    学到Page333

--- books/book6_Python核心编程（第3版）/网络编程/crawl.py
+++ books/book6_Python核心编程（第3版）/网络编程/crawl.py
@@ -7,8 +7,10 @@ from urllib.request import urlretrieve
 from urllib.parse import urlparse, urljoin
 from requests.models import InvalidURL
 
+
 class Retriever(object):
     __slots__ = ('url', 'file')
+
     def __init__(self, url):
         self.url, self.file = self.get_file(url)
 
@@ -40,19 +42,25 @@ class Retriever(object):
         f = open(self.file, 'r')
         data = f.read()
         f.close()
-        parser = HTMLParser(formatter.AbstractFormatter(formatter.DumbWriter(StringIO())))
+        parser = HTMLParser(formatter.AbstractFormatter(
+            formatter.DumbWriter(StringIO())))
         parser.feed(data)
         parser.close()
         return parser.anchorlist
+
 
 class Crawler(object):
     count = 0
 
     def __init__(self, url):
+        # q是一个待下载的链接队列。这个队列的内容在运行过程中会有变化，
+        # 有页面处理完毕就缩短，在每个下载的页面中发现新的链接则会增长。
         self.q = [url]
+        # 是所有已下载链接的一个集合
         self.seen = set()
         parsed = urlparse(url)
         host = parsed.netloc.split('@')[-1].split(':')[0]
+        # 用于存储主链接的域名，并用这个值来判定后续链接的域名与主域名是否一致
         self.dom = '.'.join(host.split('.')[-2:])
 
     def get_page(self, url, media=False):
@@ -101,6 +109,7 @@ class Crawler(object):
             url = self.q.pop()
             self.get_page(url, media)
 
+
 def main():
     if len(sys.argv) > 1:
         url = sys.argv[1]
@@ -116,6 +125,6 @@ def main():
     robot = Crawler(url)
     robot.go()
 
+
 if __name__ == '__main__':
     main()
-    
