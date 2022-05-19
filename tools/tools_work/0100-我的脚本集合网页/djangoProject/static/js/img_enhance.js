cc970bbe9ac14e611c75cc217812aa152d0242c1
公司笔记本 <694317828@qq.com>
2022-05-19 17:23:20 +0800

    learning at P14 3939

---
+++ tools/tools_work/0100-我的脚本集合网页/djangoProject/static/js/img_enhance.js
@@ -0,0 +1,4 @@
+// 给所有的图片加上点击事件
+function img_click(obj) {
+	window.open(obj.src);
+}
---
+++ tools/tools_work/0100-我的脚本集合网页/djangoProject/static/js/word_decode.js
@@ -0,0 +1,5 @@
+function word_decode(word){
+	var parser = new DOMParser();
+	var doc = parser.parseFromString(str, "text/xml");
+	return doc;
+}
