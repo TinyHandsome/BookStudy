34ecbe0c5c6426660bfc47d03ffbf166a098354e
公司笔记本 <694317828@qq.com>
2022-06-01 17:20:24 +0800

    learning at P19 5210

---
+++ tools/tools_work/0100-我的脚本集合网页/djangoProject/app/views/func0_test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# coding=utf-8 
+
+"""
+@author: Liyingjun
+@contact: 694317828@qq.com
+@software: pycharm
+@file: func0_test.py
+@time: 2022/6/1 16:22
+@desc: 
+"""
